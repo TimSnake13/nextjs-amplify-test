3c6470f77087fb5debc6d8991ff1bd18445ff4e7
GoG <forsoapfc@gmail.com>
2021-02-18 21:50:02 +0800

    basic CRUD operations without slatejs

---
+++ src/components/SlateJSNote/SlateEditor.tsx
@@ -0,0 +1,17 @@
+import React, { useCallback, useMemo, useState } from "react";
+import isHotkey from "is-hotkey";
+import { Editable, withReact, useSlate, Slate } from "slate-react";
+import {
+  Editor,
+  Transforms,
+  createEditor,
+  Node,
+  Element as SlateElement,
+} from "slate";
+import { withHistory } from "slate-history";
+
+const SlateEditor = () => {
+  return <div></div>;
+};
+
+export default SlateEditor;
